a94717aaa415b84b2e5df322e32ab744993c01ff
willsampson <wsampson@uchicago.edu>
2023-02-22 06:38:00 +0000

    final product

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,7 @@ class PhotosController < ApplicationController
   end
 
   def create
-    user_id = params.fetch("input_owner_id")
+    user_id = session.fetch(:user_id)
     image = params.fetch("input_image")
     caption = params.fetch("input_caption")
     photo = Photo.new
@@ -19,6 +19,8 @@ class PhotosController < ApplicationController
   def show
     p_id = params.fetch("the_photo_id")
     @photo = Photo.where({:id => p_id }).first
+
+    @user_id = session.fetch(:user_id)
     render({:template => "photos/details.html.erb"})
   end
 
@@ -31,6 +33,7 @@ class PhotosController < ApplicationController
   end
 
   def update
+
     id = params.fetch("the_photo_id")
     photo = Photo.where({ :id => id }).at(0)
     photo.caption = params.fetch("input_caption")
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  def authenticate
+
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+
+    user = User.where({ :username => un}).at(0)
+
+    if user == nil
+      redirect_to("/user_sign_in", { :alert => "No one here"})
+    else 
+      if user.authenticate(pw)
+        session.store(:user_id, user.id)
+      redirect_to("/", { :notice => "Welcome back"})
+      else
+      redirect_to("/user_sign_in", { :alert => "Wrong password"})
+      end
+    end
+  end
+  
+  def new_registration_form
+
+    render({ :template => "users/signup_form.html.erb"})
+  end
+  
   def index
     @users = User.all.order({ :username => :asc })
 
@@ -16,12 +40,31 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
+    save_status = user.save
 
-    redirect_to("/users/#{user.username}")
+    if save_status == true
+      session.store(:user_id, user.id)
+
+      redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!"})
+    else
+      redirect_to("/user_sign_up", { :alert => user.errors.full_messages})
+    end
   end
 
+  def toast_cookies
+    reset_session
+
+    redirect_to("/", {:notice => "See ya later"})
+  end
+
+  def new_session_form
+
+    render({ :template => "users/signin_form.html.erb"})
+  end
+  
   def update
     the_id = params.fetch("the_user_id")
     user = User.where({ :id => the_id }).at(0)
